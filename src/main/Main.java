8621c11b3269180b908cbc8d208379ef19c7b5ca
Mehdi <mehditeymorian322@gmail.com>
2021-06-24 22:13:04 +0430

    catch manager exception

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 
+import main.core.Manager;
 import main.core.NetworkConfig;
+
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        NetworkConfig config = new NetworkConfig("main/config.txt");
-        System.out.println(config);
+    public static void main(String[] args) throws IOException {
+        String fileName = "main/config.txt";
+        Manager manager = new Manager(fileName);
+        System.out.println(manager);
     }
 }
