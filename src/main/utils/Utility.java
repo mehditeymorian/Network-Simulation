757656a8fdaec5b84ef8942d4880e3a7ac2aaba8
Amirhossein <amir.h.nouri2000@gmail.com>
2021-06-24 21:12:00 +0430

    Implement dijkstra algorithm

--- src/main/utils/Utility.java
+++ src/main/utils/Utility.java
@@ -3,13 +3,54 @@ package main.utils;
 public class Utility {
 
 
-    public static int[] calculateDistances(int source , int[][] distances) {
-        // TODO: 6/24/2021 dijkstra algorithm
-        return null;
+    /**
+     * Implementation of Dijkstra algorithm.
+     *
+     * @param source Source node.
+     * @param matrix Matrix of nodes.
+     * @return Shortest distance to each node, from source.
+     */
+    public static int[] calculateDistances(int source, int[][] matrix) {
+        int numOfNodes = matrix.length;
+        int[] distances = new int[numOfNodes];
+        boolean[] isVisited = new boolean[numOfNodes];
+
+        for (int i = 0; i < numOfNodes; i++) {
+            distances[i] = Integer.MAX_VALUE;
+            isVisited[i] = false;
+        }
+
+        distances[source] = 0;
+
+        for (int i = 0; i < numOfNodes; i++) {
+            int nearestNodeIndex = minDistance(distances, isVisited);
+
+            isVisited[nearestNodeIndex] = true;
+
+            for (int j = 0; j < numOfNodes; j++) {
+                if (!isVisited[j] &&
+                        matrix[nearestNodeIndex][j] != 0 &&
+                        distances[nearestNodeIndex] != Integer.MAX_VALUE &&
+                        distances[nearestNodeIndex] + matrix[nearestNodeIndex][j] < distances[j]) {
+                    distances[j] = distances[nearestNodeIndex] + matrix[nearestNodeIndex][j];
+                }
+            }
+        }
+        return distances;
     }
 
-    private static int minDistance(int[] distance , boolean[] visited) {
-        return 0;
+    private static int minDistance(int[] distance, boolean[] visited) {
+
+        int minValue = Integer.MAX_VALUE;
+        int minIndex = -1;
+
+        for (int i = 0; i < distance.length; i++)
+            if (visited[i] == false && distance[i] <= minValue) {
+                minValue = distance[i];
+                minIndex = i;
+            }
+
+        return minIndex;
     }
 
 }
