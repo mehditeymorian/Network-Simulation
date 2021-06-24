c9f1b9ebe8b4ff4f5284bd1cd314b4029416346f
Mehdi <mehditeymorian322@gmail.com>
2021-06-24 19:54:54 +0430

    Network config class

--- src/NetworkConfig.java
+++ src/NetworkConfig.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class NetworkConfig {
+    private String fileName; // config file name
+    private int size; // routers count
+    private int[][] distances; // distances between routers
+    private List<RouterInfo> routerInfoList; // port and address of routers
+
+    public NetworkConfig(String fileName) {
+        this.fileName = fileName;
+        routerInfoList = new ArrayList<>();
+        distances = new int[size][size];
+
+    }
+
+    private void readConfigFile() {
+
+    }
+
+
+    public int distanceBetween(int from , int to) {
+        return distances[from][to];
+    }
+
+    public RouterInfo getInfo(int routerId) {
+        return routerInfoList.get(routerId);
+    }
+
+
+
 }
