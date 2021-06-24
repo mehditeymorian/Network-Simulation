9a873b73c1060b47b9245cbf0a026731ae0623de
Mehdi <mehditeymorian322@gmail.com>
2021-06-24 21:28:35 +0430

    add sample config file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 
+import main.core.NetworkConfig;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        NetworkConfig config = new NetworkConfig("main/config.txt");
+        System.out.println(config);
     }
 }
