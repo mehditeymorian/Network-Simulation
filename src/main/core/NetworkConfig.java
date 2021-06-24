cb302ed07e352bb3adf0922d2b69fb5d5d3dffe4
Mehdi <mehditeymorian322@gmail.com>
2021-06-24 21:27:35 +0430

    complete readConfig && move routerInfo to router class

--- src/main/core/NetworkConfig.java
+++ src/main/core/NetworkConfig.java
@@ -1,25 +1,63 @@
 package main.core;
 
+import main.Main;
 import main.model.RouterInfo;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class NetworkConfig {
+    private final int startPort = 1000;
+    private final String tcpAddress = "localhost";
+
     private String fileName; // config file name
     private int size; // routers count
     private int[][] distances; // distances between routers
-    private List<RouterInfo> routerInfoList; // port and address of routers
+    private List<Router> routers;
+
 
     public NetworkConfig(String fileName) {
         this.fileName = fileName;
-        routerInfoList = new ArrayList<>();
-        distances = new int[size][size];
-
+        routers = new ArrayList<>();
+        try {
+            readConfigFile();
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void readConfigFile() {
+    private void readConfigFile() throws URISyntaxException, IOException {
+        URL resource = Main.class.getClassLoader().getResource(fileName);
+        URI uri = Objects.requireNonNull(resource).toURI();
+        Path path = Paths.get(uri);
+        List<String> lines = Files.readAllLines(path);
+        // first line is routers number
+        size = Integer.parseInt(lines.get(0));
+        distances = new int[size][size];
 
+        for (int i = 1; i < lines.size(); i++) {
+            String[] split = lines.get(i).split(" ");
+            int from = Integer.parseInt(split[0]);
+            int to = Integer.parseInt(split[1]);
+            int distance = Integer.parseInt(split[2]);
+            distances[from][to] = distance;
+            distances[to][from] = distance;
+
+            if (i <= size) {
+                RouterInfo routerInfo = new RouterInfo(startPort + i - 1 , tcpAddress , startPort + i - 1 + lines.size());
+                Router router = new Router(i - 1 , routerInfo);
+                routers.add(router);
+            }
+
+        }
     }
 
 
@@ -27,10 +65,7 @@ public class NetworkConfig {
         return distances[from][to];
     }
 
-    public RouterInfo getInfo(int routerId) {
-        return routerInfoList.get(routerId);
+    public List<Router> getRouters() {
+        return routers;
     }
-
-
-
 }
--- src/main/core/Router.java
+++ src/main/core/Router.java
@@ -1,16 +1,21 @@
 package main.core;
 
+import main.model.RouterInfo;
+
 import java.net.DatagramSocket;
 import java.net.Socket;
 
 public class Router extends Thread{
     private int routerId;
+    private RouterInfo info;
+    private Socket managerSocket;
     private Socket tcpSocket;
     private DatagramSocket udpSocket;
     private SocketHandler socketHandler;
 
-    public Router(int routerId) {
+    public Router(int routerId , RouterInfo info) {
         this.routerId = routerId;
+        this.info = info;
         // TODO: 6/24/2021 init sockets
         socketHandler = new SocketHandler(tcpSocket , udpSocket);
     }
