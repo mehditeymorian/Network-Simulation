0a0a92da81cbb2ca9571bdda8a2654dc318ac6bf
Mehdi <mehditeymorian322@gmail.com>
2021-06-24 19:44:32 +0430

    Router Basic Functionality

--- src/Router.java
+++ src/Router.java
@@ -1,2 +1,25 @@
+import java.net.DatagramSocket;
+import java.net.Socket;
+
 public class Router extends Thread{
+    private int routerId;
+    private Socket tcpSocket;
+    private DatagramSocket udpSocket;
+    private SocketHandler socketHandler;
+
+    public Router(int routerId) {
+        this.routerId = routerId;
+        // TODO: 6/24/2021 init sockets
+    }
+
+
+
+
+    public String getRouterName() {
+        return String.format("Router %d\n" , getRouterId());
+    }
+
+    public int getRouterId() {
+        return routerId;
+    }
 }
